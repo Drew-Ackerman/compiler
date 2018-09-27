993638c4141187a909820c626c9acd4668eece05
Drew Ackerman <mitcha12@live.com>
2018-09-27 10:59:23 -0600

    beta algo

--- Algorithmic.py
+++ Algorithmic.py
@@ -1,6 +1,7 @@
 
 from Scanner import  TokenTypes
 import collections
+from Scanner import Token
 
 def label_number_generator():
     n = 0
@@ -66,12 +67,12 @@ class Algorithmic(object):
 
         for index, token in enumerate(self.expression):
             if token.token_type in token_types:
-                return self.expression.pop(index)
+                return self.expression[index]
 
     def process_postfix(self):
-        self.setup_temp()
+        #self.setup_temp()
 
-        while self.expression:
+        while len(self.expression) > 1:
             self._process_postfix(self.expression)
 
     def setup_temp(self):
@@ -84,68 +85,125 @@ class Algorithmic(object):
 
     def _process_postfix(self, expression):
 
-        operand = self.get_from_expression(TokenTypes.NUM, TokenTypes.VARIABLE)
+        #operand = self.get_from_expression(TokenTypes.NUM, TokenTypes.VARIABLE)
         operator = self.get_from_expression(TokenTypes.ALGORITHMIC)
+        operator_index = expression.index(operator)
+        self.expression.pop(operator_index)
 
+        operand_right = expression.pop(operator_index-1)
+        operand_left = expression.pop(operator_index-2)
 
-        if operand is None or operator is None:
-            raise ValueError("operand is {}, operator is {}".format(operand, operator))
+        if operand_right is None or operand_left is None:
+            raise ValueError("operand is {}, operator is {}".format(operand_right, operand_left))
 
-        self.process_expression(operand, operator)
+        new_token = self.process_expression(operand_left, operand_right, operator)
 
-    def process_expression(self, operand, operator):
+        self.expression.insert(operator_index-2, new_token)
+
+    def process_expression(self, opl, opr, operator):
 
         if operator.token_str == "+":
-            self.add(operand)
+            return self.add(opl, opr)
         elif operator.token_str == "-":
-            self.sub(operand)
+            return self.sub(opl, opr)
         elif operator.token_str == "*":
-            self.mult(operand)
+            return self.mult(opl, opr)
         elif operator.token_str == "^":
-            self.pow(operand)
+            return self.pow(opl, opr)
+
+
+    def add(self, opl, opr):
+
+        if opl.token_type == TokenTypes.VARIABLE:
+            self.asm_string.append("mov edi, DWORD[{}];".format(opl.token_str))
+        else:
+            self.asm_string.append("mov edi, {};".format(opl.token_str))
+
+        if opr.token_type == TokenTypes.VARIABLE:
+            self.asm_string.append("add edi, DWORD[{}];".format(opr.token_str))
+        else:
+            self.asm_string.append("add edi, {};".format(opr.token_str))
 
         self.asm_string.append("mov DWORD[temp_0], edi;")
 
-    def add(self, operand):
-        self.asm_string.append("mov edi, DWORD[temp_0];")
-        if operand.token_type == TokenTypes.VARIABLE:
-            self.asm_string.append("add edi, DWORD[{}];".format(operand.token_str))
-        else:
-            self.asm_string.append("add edi, {};".format(operand.token_str))
+        new_num_value = str(int(opl.token_str) + int(opr.token_str))
+        new_token = Token(TokenTypes.NUM)
+        new_token.token_str = new_num_value
+        return new_token
 
-    def sub(self, operand):
-        self.asm_string.append("mov edi, DWORD[temp_0];")
-        if operand.token_type == TokenTypes.VARIABLE:
-            self.asm_string.append("sub edi, DWORD[{}];".format(operand.token_str))
-        else:
-            self.asm_string.append("sub edi, {};".format(operand.token_str))
 
-    def mult(self, operand):
-        self.asm_string.append("mov edi, DWORD[temp_0];")
-        if operand.token_type == TokenTypes.VARIABLE:
-            self.asm_string.append("imul edi, DWORD[{}];".format(operand.token_str))
-        else:
-            self.asm_string.append("imul edi, {};".format(operand.token_str))
+    def sub(self, opl, opr):
 
-    def pow(self, operand):
+        if opl.token_type == TokenTypes.VARIABLE:
+            self.asm_string.append("mov edi, DWORD[{}];".format(opl.token_str))
+        else:
+            self.asm_string.append("mov edi, {};".format(opl.token_str))
+
+        if opr.token_type == TokenTypes.VARIABLE:
+            self.asm_string.append("sub edi, DWORD[{}];".format(opr.token_str))
+        else:
+            self.asm_string.append("sub edi, {};".format(opr.token_str))
+
+        self.asm_string.append("mov DWORD[temp_0], edi;")
+
+
+        new_num_value = str(int(opl.token_str) - int(opr.token_str))
+        new_token = Token(TokenTypes.NUM)
+        new_token.token_str = new_num_value
+        return new_token
+
+    def mult(self, opl, opr):
+
+        if opl.token_type == TokenTypes.VARIABLE:
+            self.asm_string.append("mov edi, DWORD[{}];".format(opl.token_str))
+        else:
+            self.asm_string.append("mov edi, {};".format(opl.token_str))
+
+        if opr.token_type == TokenTypes.VARIABLE:
+            self.asm_string.append("imul edi, DWORD[{}];".format(opr.token_str))
+        else:
+            self.asm_string.append("imul edi, {};".format(opr.token_str))
+
+        self.asm_string.append("mov DWORD[temp_0], edi;")
+
+        if opl.token_type is TokenTypes.VARIABLE:
+            opl = self.symbol_table.lookup(opl).token_str
+
+
+        new_num_value = str(int(opl.token_str) * int(opr.token_str))
+        new_token = Token(TokenTypes.NUM)
+        new_token.token_str = new_num_value
+        return new_token
+
+    def pow(self, opl, opr):
         label_string_num = next(self.label_num_gen)
         self.asm_string.append("xor edi, edi")	#; clear out the counter
         self.asm_string.append("inc edi")
         self.asm_string.append("mov eax, DWORD[temp_0]")
         self.asm_string.append("_exp_top_{}:".format(label_string_num))
 
-        if operand.token_type == TokenTypes.VARIABLE:
-            self.asm_string.append("cmp edi, DWORD[{}];".format(operand.token_str))
+        if opl.token_type == TokenTypes.VARIABLE:
+            self.asm_string.append("cmp edi, DWORD[{}];".format(opr.token_str))
         else:
-            self.asm_string.append("cmp edi, {};".format(operand.token_str))
+            self.asm_string.append("cmp edi, {};".format(opr.token_str))
 
         self.asm_string.append("jz _exp_out_{}".format(label_string_num))       #; jump out if done
-        self.asm_string.append("imul eax, DWORD[temp_0]")
+
+        if opr.token_type == TokenTypes.VARIABLE:
+            self.asm_string.append("imul eax, DWORD[{}];".format(opl.token_str))
+        else:
+            self.asm_string.append("imul eax, {};".format(opl.token_str))
+
         self.asm_string.append("inc	edi")
         self.asm_string.append("jmp	_exp_top_{}".format(label_string_num))      #; result of exponentiation is in eax
         self.asm_string.append("_exp_out_{}:".format(label_string_num))
         self.asm_string.append("mov	DWORD[temp_0],	eax")
-        self.asm_string.append("mov edi, DWORD[temp_0]")
+
+        new_num_value = str(int(opl.token_str) ^ int(opr.token_str))
+        new_token = Token(TokenTypes.NUM)
+        new_token.token_str = new_num_value
+        return new_token
+
     def get_priority(self, token_str):
             if token_str in ["+",  "-"]:
                 return 1
